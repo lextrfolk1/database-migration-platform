4f7c1949cccda7189e35f37a97dc34b8c4f82241
lextrfolk1 <lextrfolk1@gmail.com>
2026-06-15 11:48:06 +0530

    initialization schema

--- src/main/java/com/lextr/migrationplatform/exception/FlywayExecutionException.java
+++
@@ -1,8 +0,0 @@
-package com.lextr.migrationplatform.exception;
-
-public class FlywayExecutionException extends MigrationExecutionException {
-
-    public FlywayExecutionException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
